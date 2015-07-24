2d0b81b42e07a82d683512a3f90f69d7bb6ba6b6
matsukov-ea <matsukov-ea@fxclub.org>
2015-07-24 14:52:13 +0300

    migrating to personal routing;

---
+++ AspectJ-gradle/src/main/groovy/com/archinamon/AspectjCompileTask.groovy
@@ -0,0 +1,84 @@
+package com.archinamon
+
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.TaskAction
+
+class AspectjCompileTask extends DefaultTask {
+
+    FileCollection aspectpath
+    File destinationDir
+    FileCollection classpath
+    String bootclasspath
+    def sourceroots
+
+    @TaskAction
+    def compile() {
+
+        final def log = project.logger
+
+        //http://www.eclipse.org/aspectj/doc/released/devguide/ajc-ref.html
+        //
+        // -sourceRoots:
+        //  Find and build all .java or .aj source files under any directory listed in DirPaths. DirPaths, like classpath, is a single argument containing a list of paths to directories, delimited by the platform- specific classpath delimiter. Required by -incremental.
+        // -inpath:
+        //  Accept as source bytecode any .class files in the .jar files or directories on Path. The output will include these classes, possibly as woven with any applicable aspects. Path is a single argument containing a list of paths to zip files or directories, delimited by the platform-specific path delimiter.
+        // -classpath:
+        //  Specify where to find user class files. Path is a single argument containing a list of paths to zip files or directories, delimited by the platform-specific path delimiter.
+        // -aspectPath:
+        //  Weave binary aspects from jar files and directories on path into all sources. The aspects should have been output by the same version of the compiler. When running the output classes, the run classpath should contain all aspectpath entries. Path, like classpath, is a single argument containing a list of paths to jar files, delimited by the platform- specific classpath delimiter.
+        // -bootclasspath:
+        //  Override location of VM's bootclasspath for purposes of evaluating types when compiling. Path is a single argument containing a list of paths to zip files or directories, delimited by the platform-specific path delimiter.
+        // -d:
+        //  Specify where to place generated .class files. If not specified, Directory defaults to the current working dir.
+        // -preserveAllLocals:
+        //  Preserve all local variables during code generation (to facilitate debugging).
+
+        def sourceRoots = []
+        sourceroots.sourceCollections.each {
+            it.asFileTrees.each {
+                sourceRoots << it.dir
+            }
+        }
+
+        def String[] args = [
+                "-showWeaveInfo",
+                //"-incremental",
+                "-encoding", "UTF-8",
+                "-" + project.android.compileOptions.sourceCompatibility.toString(),
+                "-inpath", destinationDir.absolutePath,
+                "-aspectpath", aspectpath.asPath,
+                "-d", destinationDir.absolutePath,
+                "-classpath", classpath.asPath,
+                "-bootclasspath", bootclasspath,
+                "-sourceroots", sourceRoots.join(File.pathSeparator)
+        ]
+
+        log.debug "ajc args: " + Arrays.toString(args)
+
+        MessageHandler handler = new MessageHandler(true);
+        new Main().run(args, handler);
+        for (IMessage message : handler.getMessages(null, true)) {
+            switch (message.getKind()) {
+                case IMessage.ABORT:
+                case IMessage.ERROR:
+                case IMessage.FAIL:
+                    log.error message.message, message.thrown
+                    throw new GradleException(message.message, message.thrown)
+                case IMessage.WARNING:
+                    log.warn message.message, message.thrown
+                    break;
+                case IMessage.INFO:
+                    log.info message.message, message.thrown
+                    break;
+                case IMessage.DEBUG:
+                    log.debug message.message, message.thrown
+                    break;
+            }
+        }
+    }
+}

d6249d40665cb46d316fb55fc1ddfc589784ba96
archinamon <archinamon@yandex-team.ru>
2016-04-26 12:08:19 +0300

    trying to apply unit test task dependency;

--- AspectJ-gradle/src/main/groovy/com/archinamon/AndroidAspectJPlugin.groovy
+++ AspectJ-gradle/src/main/groovy/com/archinamon/AndroidAspectJPlugin.groovy
@@ -165,14 +165,14 @@ class AndroidAspectJPlugin implements Plugin<Project> {
                     retrolambda.dependsOn compileAspectTask;
                 }
 
-                //apply behavior
-                project.tasks["compile${variantName}Ndk"].dependsOn compileAspectTask;
-
                 //trying to apply aj-task after ${variant}UnitTestJava task
                 JavaCompile compileUnitTest = (JavaCompile) project.tasks.findByName("compile${variantName}UnitTestJavaWithJavac")
                 if (compileUnitTest) {
                     project.logger.warn "Configuring AspectJ compile task for UnitTest task";
                     compileUnitTest.mustRunAfter(compileAspectTask);
+                } else {
+                    //apply behavior
+                    project.tasks["compile${variantName}Ndk"].dependsOn compileAspectTask;
                 }
             }
         }
